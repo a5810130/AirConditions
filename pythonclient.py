ac178aa64123e08ea84de7c1a3d2d97a90d84487
nitikarn <nitikarn95@gmail.com>
2018-10-25 15:14:46 +0700

    Call DeliveryTask by python client

--- pythonclient.py
+++ pythonclient.py
@@ -6,22 +6,29 @@ from lxml import etree
 
 client = Client('http://localhost/airconditions/server.php?wsdl')
 
+# InsertAirInfo
 # client.service.InsertAirInfo(room = "400",unixtime = 17775,temperature = 25.9,humidity = 55.5)
 
-res = client.service.GetStudentInfo()
+# GetStudentInfo
+# res = client.service.GetStudentInfo()
 # res = client.create_message(client.service, 'GetStudentInfo')
+#     # Validate schema
+# xsd_tree = etree.parse("http://localhost/AirConditions/Students.xsd")
+# xml_root = etree.XML(res.encode('utf-8'))
+# schema = etree.XMLSchema(xsd_tree)
+# print(schema.validate(xml_root))
+# out = (etree.tostring(xml_root)).decode('utf-8')
+# print(out)
 
-xsd_tree = etree.parse("http://localhost/AirConditions/Students.xsd")
+# InsertTask
+client.service.InsertTask(name="Bob", address="72/3 Bangkok", weight=5.2)
+# TaskDelivered
+client.service.TaskDelivered(id=5);
 
-xml_root = etree.XML(res.encode('utf-8'))
-schema = etree.XMLSchema(xsd_tree)
 
-print(schema.validate(xml_root))
 
-out = (etree.tostring(xml_root)).decode('utf-8')
-print(out)
-# root = ET.fromstring(res)
-# for i in root:
-#     print(i)
+
+
+
 
 
